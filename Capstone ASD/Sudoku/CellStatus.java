d9fd788aa72cb24ee623fdfd32ea21ddf8222323
Gabskie69 <gggabriel9327@gmail.com>
2024-12-06 15:45:34 +0700

    First Init

---
+++ Capstone ASD/Sudoku/CellStatus.java
@@ -0,0 +1,14 @@
+package Sudoku;
+
+public enum CellStatus {
+    /**
+     * An enumeration of constants to represent the status
+     * of each cell.
+     */
+    GIVEN,         // clue, no need to guess
+    TO_GUESS,      // need to guess - not attempted yet
+    CORRECT_GUESS, // need to guess - correct guess
+    WRONG_GUESS    // need to guess - wrong guess
+    // The puzzle is solved if none of the cells have
+    //  status of TO_GUESS or WRONG_GUESS
+}
