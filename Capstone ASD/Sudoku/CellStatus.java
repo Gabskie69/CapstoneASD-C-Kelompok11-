51611d2cc65d7a9333416517f8220cd5eeca07ab
Gabriel Hadi Melvanto Sihaloho <141644188+Gabskie69@users.noreply.github.com>
2024-12-19 23:58:18 +0700

    Update CellStatus.java

--- Capstone ASD/Sudoku/CellStatus.java
+++ Capstone ASD/Sudoku/CellStatus.java
@@ -1,32 +1,32 @@
-///**
-// * ES234317-Algorithm and Data Structures
-// * Semester Ganjil, 2024/2025
-// * Group Capstone Project
-// * Group #11
-// * 1 - 5026231036 - Shafly Hidayatullah
-// * 2 - 5026231071 - Aryabima Kurnia Pratama Santoso
-// * 3 - 5026231189 - Gabriel Hadi Melvanto Sihaloho
-// */
-///**
-// * ES234317-Algorithm and Data Structures
-// * Semester Ganjil, 2024/2025
-// * Group Capstone Project
-// * Group #11
-// * 1 - 5026231036 - Shafly Hidayatullah
-// * 2 - 5026231071 - Aryabima Kurnia Pratama Santoso
-// * 3 - 5026231189 - Gabriel Hadi Melvanto Sihaloho
-// */
-//package Sudoku;
-//
-//public enum CellStatus {
-//    /**
-//     * An enumeration of constants to represent the status
-//     * of each cell.
-//     */
-//    GIVEN,         // clue, no need to guess
-//    TO_GUESS,      // need to guess - not attempted yet
-//    CORRECT_GUESS, // need to guess - correct guess
-//    WRONG_GUESS    // need to guess - wrong guess
-//    // The puzzle is solved if none of the cells have
-//    //  status of TO_GUESS or WRONG_GUESS
-//}
+/**
+* ES234317-Algorithm and Data Structures
+* Semester Ganjil, 2024/2025
+* Group Capstone Project
+* Group #11
+* 1 - 5026231036 - Shafly Hidayatullah
+* 2 - 5026231071 - Aryabima Kurnia Pratama Santoso
+* 3 - 5026231189 - Gabriel Hadi Melvanto Sihaloho
+*/
+/**
+* ES234317-Algorithm and Data Structures
+* Semester Ganjil, 2024/2025
+* Group Capstone Project
+* Group #11
+* 1 - 5026231036 - Shafly Hidayatullah
+* 2 - 5026231071 - Aryabima Kurnia Pratama Santoso
+* 3 - 5026231189 - Gabriel Hadi Melvanto Sihaloho
+*/
+package Sudoku;
+
+public enum CellStatus {
+   /**
+    * An enumeration of constants to represent the status
+    * of each cell.
+    */
+   GIVEN,         // clue, no need to guess
+   TO_GUESS,      // need to guess - not attempted yet
+   CORRECT_GUESS, // need to guess - correct guess
+   WRONG_GUESS    // need to guess - wrong guess
+   // The puzzle is solved if none of the cells have
+   //  status of TO_GUESS or WRONG_GUESS
+}
