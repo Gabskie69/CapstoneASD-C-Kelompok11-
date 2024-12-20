6e8658443cb65580e75fca85b72b76b528ad6403
Gabriel Hadi Melvanto Sihaloho <141644188+Gabskie69@users.noreply.github.com>
2024-12-20 18:51:14 +0700

    Update final TicTacToe

--- TicTacToe/TicTacToe/Cell.java
+++ TicTacToe/TicTacToe/Cell.java
@@ -1,3 +1,12 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #11
+ * 1 - 5026231036 - Shafly Hidayatullah
+ * 2 - 5026231071 - Aryabima Kurnia Pratama Santoso
+ * 3 - 5026231189 - Gabriel Hadi Melvanto Sihaloho
+ */
 package TicTacToe;
 import java.awt.*;
 /**
